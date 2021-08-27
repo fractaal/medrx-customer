8be41c3b1fca20938043a22c19ced494e2ff602e
lcsantos11395 <lcsantos11395@gmail.com>
2021-08-27 23:19:49 +0800

    Main Menu layout & pages practice (front-end only)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,49 +1,60 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class='bg-body'>
+    <q-footer>
+      <q-tabs
+            v-model="tab"
+            dense
+            class="navigation-menu"
+          >
+            <q-route-tab name="mails" label="Shop">
+              <img src='../../public/icons/shop.svg'>
+            </q-route-tab>
+            <q-route-tab name="alarms" label="Prescription">
+              <img src='../../public/icons/upload.svg'>
+            </q-route-tab>
+            <q-route-tab name="movies" label="Soon">
+              <img src='../../public/icons/soon.svg'>
+            </q-route-tab>
+          </q-tabs>
+    </q-footer>
+      
+    
   </q-page>
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
-import { defineComponent, ref } from 'vue';
+<script>
+import { defineComponent } from 'vue';
+import { ref } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
+  setup () {
+    return {
+      tab: ref('mails')
+    }
   }
-});
+})
+
+
 </script>
+
+<style scoped>
+.bg-body {
+  background: #97f4e6;
+}
+
+
+.shadow-box {
+  width: 90px;
+  height: 90px;
+  margin: 25px;
+  font-size: 12px;
+}
+
+.navigation-menu {
+  background: #6ae0ce;
+  color: #ffffff;
+
+}
+
+</style>
