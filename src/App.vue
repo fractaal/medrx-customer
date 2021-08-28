564c93ee55df9ab8bb65413af1b6b071f4c8980a
Ben Jude <benedict.jude.rocat@gmail.com>
2021-08-28 21:23:16 +0800

    :sparkles: Start Firebase on App startup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { } from 'src/firebase' // Run the code inside firebase.ts by importing it
 
 export default defineComponent({
   name: 'App'
