22c1889c5ba8a9447e38cbe38f3df5088b60124c
Ben Jude <benedict.jude.rocat@gmail.com>
2021-08-31 01:39:49 +0800

    :construction: Component testing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <q-page class='bg-body'>
+      <div class="font-black text-2xl p-4 -mb-10">If you want to live...</div>
+      <div class="p-4 -mb-4">Buy these essential medical products! <i>Or die.</i></div>
+      <horizontal-scroller>
+        <product-card v-for="i in 100" :key="i" name="Name" description="Test" :price="69.420"/>
+      </horizontal-scroller>
     <q-footer>
       <q-tabs
             v-model="tab"
@@ -17,17 +22,18 @@
             </q-route-tab>
           </q-tabs>
     </q-footer>
-      
-    
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import { ref } from 'vue';
+import ProductCard from 'src/components/ProductCard.vue';
+import HorizontalScroller from 'src/components/HorizontalScroller.vue';
 
 export default defineComponent({
   name: 'PageIndex',
+  components: { ProductCard, HorizontalScroller },
   setup () {
     return {
       tab: ref('mails')
