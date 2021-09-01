d7b89dc0fc2ecd68c8c4cd2823a1cc4b1e988e26
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-01 23:36:16 +0800

    :wrench: Rule was missing an 's'

--- .eslintrc.js
+++ .eslintrc.js
@@ -85,7 +85,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/restrict-template-expression': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
