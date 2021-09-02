d07bb896b789c1407e8765745f34d2cc3242dc20
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-02 20:23:26 +0800

    :sparkles: Auto login if existing auth session is present

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,23 @@
   <router-view />
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { } from 'src/firebase' // Run the code inside firebase.ts by importing it
+import { defineComponent, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { getAuth } from 'firebase/auth'
+import { init } from 'src/firebase'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup () {
+    init()
+    
+    const router = useRouter()
+
+    getAuth().onAuthStateChanged(user => {
+      if (user) {
+        router.push('/home')
+      }
+    })
+  }
 })
 </script>
