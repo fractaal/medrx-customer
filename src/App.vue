9376031250424025ee8220984fe57d1156964815
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-04 20:23:34 +0800

    :sparkles: Splash Screen, Animations, Status Bar Styling

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
-  <router-view />
+  <transition
+    enter-active-class="animated fadeIn"
+    leave-active-class="animated fadeOut"
+    appear
+    :duration="1000"
+  >
+    <router-view />
+  </transition>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
 import { getAuth } from 'firebase/auth'
 import { init } from 'src/api/firebase'
+import { Plugins, StatusBarStyle } from '@capacitor/core'
+
+function useStatusBar() {
+  if (!Plugins) return
+  const { StatusBar } = Plugins
+  if (!StatusBar) return
+  console.log(StatusBar)
+  StatusBar.setBackgroundColor({color: '#ffffff'})
+  StatusBar.setStyle({style: StatusBarStyle.Light})
+}
 
 export default defineComponent({
   name: 'App',
   setup () {
     init()
+    if (process.env.MODE === 'capacitor') {
+      useStatusBar()
+    }
     
     const router = useRouter()
 
     getAuth().onAuthStateChanged(user => {
       if (user) {
         router.push('/home')
+      } else {
+        router.push('/login')
       }
     })
   }
