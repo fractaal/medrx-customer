019fcd11d4b614af7707d6e958c36be4ded13e7b
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-04 20:27:08 +0800

    :sparkles: Splash Screen!

---
+++ src/pages/Splash.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="min-h-screen">
+    <div class="pt-32">
+      <video playsinline loop autoplay style="width: 75vw;" :style="style" @loadeddata="show" class="mx-auto">
+        <source src="~assets/MedRx.webm"/>
+      </video>
+      <q-spinner size="32px" :thickness="8" class="mx-auto"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Splash',
+  setup() {
+    const style = ref('display: none;')
+
+    return {
+      style,
+      show: () => style.value = 'display: block;'
+    }
+  }
+})
+</script>
