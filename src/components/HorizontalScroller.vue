468dc0974def3c31f917356bb33c1e53b40c0af2
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-04 20:25:11 +0800

    :lipstick: spacing

--- src/components/HorizontalScroller.vue
+++ src/components/HorizontalScroller.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-row flex-nowrap p-4 space-x-6 overflow-x-scroll overflow-y-hidden">
+    <div class="flex flex-row flex-nowrap p-4 space-x-4 overflow-x-scroll overflow-y-hidden">
 		<slot/>
 	</div>
 </template>
