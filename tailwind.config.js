70a51835c8b38e74265ae732f031cecad96fd142
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-06 02:51:30 +0800

    :wrench: Purge unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'src/components/**/*.vue',
+      'src/layouts/**/*.vue',
+      'src/pages/**/*.vue',
+      'src/App.vue',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
