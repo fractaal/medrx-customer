ce817fa0a3fbdc85d24ceb65ea29e559ff3c1fb9
lcsantos11395 <lcsantos11395@gmail.com>
2021-09-05 22:53:56 +0800

    :construction: Settings (Change Language, logout)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,58 +3,23 @@
 
     <q-header unelevated class="px-2 top mx-auto w-full md:w-3/4 lg:w-3/5 py-4">
       <q-toolbar>
-        <q-btn dense flat round @click="toggleLeftDrawer">
+
+        <q-btn dense flat round @click="$router.push('/settings')">
           <q-avatar color="primary" size="48px" class="shadow-xl">
             <img :src="`https://avatars.dicebear.com/api/micah/${seed}.svg`"/>
           </q-avatar>
         </q-btn>
-        <div class="ml-4 text-h6 font-black">Hello, PENIS!</div>
 
-        <q-toolbar-title style="text-align:center">
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          
-        </q-toolbar-title>
+        <div class="ml-4 text-h6 font-black">{{$t('Hello')}}, PENIS!</div>
 
-         <q-btn dense flat round @click="toggleRightDrawer">
+        <q-space />
+
+        <q-btn dense flat round @click="toggleRightDrawer">
           <q-icon name="shopping_cart" size="2rem" class="p-4"/>
         </q-btn>
+
       </q-toolbar>
     </q-header>
-
-    <q-drawer overlay v-model="leftDrawerOpen" side="left">
-      <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
-          <q-list padding>
-            <q-item
-              clickable
-              v-ripple
-              :active="link === 'inbox'"
-              @click="$router.push('/settings')"
-              active-class="my-menu-link"
-            >
-            <q-item-section avatar>
-              <q-icon name="settings" />
-            </q-item-section>
-
-            <q-item-section>Settings</q-item-section>
-            </q-item>
-
-          </q-list>
-        </q-scroll-area>
-
-        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
-          <div class="absolute-bottom bg-transparent">
-            <q-btn dense flat round>
-              <q-avatar color="primary" size="48px" @click="randomizeSeed" class="shadow-xl">
-                <img :src="`https://avatars.dicebear.com/api/micah/${seed}.svg`"/>
-              </q-avatar>
-            </q-btn>
-            <div class="text-weight-bold">Razvan Stoenescu</div>
-            <div>@rstoenescu</div>
-          </div>
-        </q-img>
-      </q-drawer>
 
     <q-page-container>
       <transition
@@ -76,13 +41,16 @@
             <q-route-tab name="mails" label="Shop" to='/home'>
               <img src='../../public/icons/shop.svg'>
             </q-route-tab>
+
             <q-route-tab name="alarms" label="Prescription" to='/upload'>
               <img src='../../public/icons/upload.svg'>
             </q-route-tab>
+
             <q-route-tab name="movies" label="Soon">
               <img src='../../public/icons/soon.svg'>
             </q-route-tab>
-          </q-tabs>
+
+      </q-tabs>
     </q-footer>
 
   </q-layout>
@@ -94,17 +62,11 @@ import { seed, randomizeSeed } from 'src/api/seed'
 
 export default {
   setup () {
-    const leftDrawerOpen = ref(false)
 
     return {
       tab: ref('mails'),
       randomizeSeed,
       seed,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      },
-
       toggleRightDrawer () {
         // TODO
       }
--- src/pages/settingPage.vue
+++ src/pages/settingPage.vue
@@ -1,23 +1,87 @@
 <template>
+
  <q-page class='bg-body'>
-     <div class="q-pa-md" style="max-width: 800px">
-      <q-list bordered padding>
-      <q-item-label header>User Controls</q-item-label>
+  <div>
+           <q-img class="top" src="https://cdn.quasar.dev/img/material.png" style="height: 140px">
+              <div class="absolute-bottom bg-transparent">
 
-      <q-item clickable v-ripple>
-          
-      </q-item>
+                <q-btn dense flat round >
+                  <q-avatar color="primary" size="70px" @click="randomizeSeed" class="shadow-xl">
+                    <img :src="`https://avatars.dicebear.com/api/micah/${seed}.svg`"/>
+                  </q-avatar>
+                </q-btn>
 
-    </q-list>
+                <div class="text-weight-bold">Oten bwisit</div>
+                <div>@bigpipienergy</div>
+                
+              </div>
+            </q-img>      
+
+
+      <q-list bordered separator>
+
+        <q-item-label header>Profile</q-item-label>
+
+        <q-item-label header>User Controls</q-item-label>
+          <q-item clickable v-ripple @click="chlang = true">
+            <q-item-section>Current Language: {{ locale }} </q-item-section>
+          </q-item>
+
+            <q-dialog v-model="chlang">
+              <q-card>
+                <q-list bordered separator>
+
+                  <q-item clickable v-ripple style="min-width: 300px">
+                    <q-item-section v-model='locale' @click="locale='en-US'; chlang = false">English</q-item-section>
+                  </q-item>
+
+                  <q-item clickable v-ripple>
+                    <q-item-section v-model='locale' @click="locale='TGL';chlang = false">Filipino</q-item-section>
+                  </q-item>
+
+                </q-list>
+
+              </q-card>
+            </q-dialog>
+            
+          <q-item clickable v-ripple @click="logout">
+            <q-item-section>Log-out</q-item-section>
+          </q-item>
+
+
+      </q-list>
+
     </div>
  </q-page>
-</template>
+</template> 
 
-<script>
+<script lang="ts">
+import { seed, randomizeSeed } from 'src/api/seed'
+import { useI18n } from 'vue-i18n'
+import { ref } from 'vue'
+import { getAuth, signOut } from "firebase/auth";
+
 export default {
   setup () {
+
+    const { locale } = useI18n({ useScope: 'global' })
+    const auth = getAuth();
+
+    const logout = async () => {
+      await signOut(auth)
+    }
+    // Sign-out successful.
+
     return {
-      
+      randomizeSeed,
+      seed,
+      locale,
+      localeOptions: [
+        { value: 'en-US', label: 'English' },
+        { value: 'TGL', label: 'Filipino' }
+      ],
+      chlang: ref(false),
+      logout
     }
   }
 }
