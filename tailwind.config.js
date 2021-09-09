d76afc8452e5f9731515b5d9f97c83a70e956b24
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-10 03:25:09 +0800

    :wrench: Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    colors: {
-      'MedRx_theme': '#30b59e',
-    },
-    extend: {},
+    extend: {
+      colors: {
+        'medrx': '#30b59e',
+      },
+    }
   },
   variants: {
     extend: {},
