11a48c56142db604f2d620124a53e31779bc0737
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-10 00:01:24 +0800

    :construction: new ListItem component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-item clickable v-ripple style="min-width: 300px">
+    <q-item-section v-model="locale" @click="locale = 'en-US'; chlang = false">English</q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+  name: 'ListItem',
+  setup() {
+    const { locale } = useI18n({ useScope: 'global' })
+    return {
+      locale
+    }
+  }
+})
+</script>
