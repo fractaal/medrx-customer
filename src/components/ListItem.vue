76fe871bbf2c1c95cb82af1e616a3407711b5930
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-10 03:25:33 +0800

    :construction: ListItem iteration

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,20 +1,15 @@
 <template>
-  <q-item clickable v-ripple style="min-width: 300px">
-    <q-item-section v-model="locale" @click="locale = 'en-US'; chlang = false">English</q-item-section>
+  <q-item clickable class="p-4 rounded-xl border border-gray-200">
+    <q-item-section>
+      <slot />
+    </q-item-section>
   </q-item>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
-  name: 'ListItem',
-  setup() {
-    const { locale } = useI18n({ useScope: 'global' })
-    return {
-      locale
-    }
-  }
+  name: 'ListItem'
 })
 </script>
