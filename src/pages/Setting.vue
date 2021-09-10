70682d54b90661a92dee376564a95a57c4aafd1a
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-10 10:44:22 +0800

    :construction: Extensive UI improvements

--- src/pages/Setting.vue
+++ src/pages/Setting.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-page class="bg-body">
+  <q-page>
     <div>
-      <q-img class="top" src="https://cdn.quasar.dev/img/material.png" style="height: 130px">
+      <q-img class="top rounded-b-2xl" src="https://64.media.tumblr.com/85ac3c19f74c65386969162b12e24f46/tumblr_inline_o29dnbbdq41rtb73a_1280.jpg" style="height: 130px">
         <div class="absolute-bottom bg-transparent">
           <q-btn round dense flat>
             <q-avatar color="primary" size="70px" @click="randomizeSeed" class="shadow-xl">
-              <img :src="`https://avatars.dicebear.com/api/micah/${seed}.svg`" />
+              <q-img :src="`https://avatars.dicebear.com/api/micah/${seed}.svg`" />
             </q-avatar>
           </q-btn>
 
@@ -13,13 +13,30 @@
         </div>
       </q-img>
 
-      <q-list bordered separator>
-        <q-item-label header>Profile</q-item-label>
+      <q-list>
+        <q-item-label header overline class="font-black">PROFILE</q-item-label>
 
-        <q-item-label header>User Controls</q-item-label>
-        <q-item clickable @click="chlang = true">
-          <q-item-section>Current Language: {{ locale }}</q-item-section>
-        </q-item>
+        <list-item color="primary" name="person" size="2rem">
+          Profile Picture
+        </list-item>        
+
+        <list-item color="primary" name="badge" size="2rem">
+          Name
+        </list-item>        
+
+        <list-item color="primary" name="call" size="2rem">
+          Phone Numbers
+        </list-item>
+
+        <list-item color="primary" name="home" size="2rem">
+          Delivery Addresses
+        </list-item>
+
+        <q-item-label header overline class="font-black">USER CONTROLS</q-item-label>
+
+        <list-item @click="chlang = true" color="primary" name="language" size="2rem">
+          Current Language: {{ locale }}
+        </list-item>
 
         <q-dialog v-model="chlang">
           <q-card>
@@ -31,13 +48,13 @@
           </q-card>
         </q-dialog>
 
-        <q-item clickable v-ripple @click="confirm = true">
-          <q-item-section>Log-out</q-item-section>
-        </q-item>
+        <list-item @click="confirm = true" color="primary" name="logout" size="2rem">
+          Log Out
+        </list-item>
 
         <q-dialog v-model="confirm" persistent>
           <q-card>
-            <q-list bordered separator>
+            <q-list>
               <q-card-section class="row items-center">
                 <q-avatar icon="warning" color="primary" text-color="white" />
                 <span class="q-ml-sm">Are you sure you want to log-out?</span>
@@ -69,7 +86,10 @@ import { ref } from 'vue'
 import { getAuth, signOut } from 'firebase/auth';
 import { getUser } from 'src/api/firebase';
 
+import ListItem from 'src/components/ListItem.vue';
+
 export default {
+  components: { ListItem },
   setup() {
 
     const { locale } = useI18n({ useScope: 'global' })
