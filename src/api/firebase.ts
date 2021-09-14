9c759007882d9729db2238e74bcf7bb5cde9f828
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-14 20:37:39 +0800

    :recycle: Format code & use serverTimestamp function instead for registrationDate

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,10 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { FirebaseApp, initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getFirestore, setDoc, doc, collection, Firestore } from 'firebase/firestore'
-import { Database, getDatabase } from 'firebase/database'
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, browserLocalPersistence } from 'firebase/auth';
-import { Notify } from 'quasar'
+import {
+  getFirestore,
+  setDoc,
+  doc,
+  collection,
+  Firestore,
+  serverTimestamp,
+} from 'firebase/firestore';
+import { Database, getDatabase } from 'firebase/database';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  browserLocalPersistence,
+} from 'firebase/auth';
+import { Notify } from 'quasar';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,55 +31,62 @@ const firebaseConfig = {
   messagingSenderId: '535999064122',
   appId: '1:535999064122:web:06cced6ce33d009b22ed03',
   measurementId: 'G-G2Y85HXLNM',
-  databaseUrl: 'https://medrx-test-default-rtdb.asia-southeast1.firebasedatabase.app/'
+  databaseUrl:
+    'https://medrx-test-default-rtdb.asia-southeast1.firebasedatabase.app/',
 };
 
-export let app: FirebaseApp
-export let firestore: Firestore
-export let database: Database
+export let app: FirebaseApp;
+export let firestore: Firestore;
+export let database: Database;
 
 export const init = () => {
   app = initializeApp(firebaseConfig);
-  firestore = getFirestore(app)
-  database = getDatabase(app, 'https://medrx-test-default-rtdb.asia-southeast1.firebasedatabase.app/')
+  firestore = getFirestore(app);
+  database = getDatabase(
+    app,
+    'https://medrx-test-default-rtdb.asia-southeast1.firebasedatabase.app/'
+  );
   getAnalytics(app);
-  getAuth().setPersistence(browserLocalPersistence)
-}
+  getAuth().setPersistence(browserLocalPersistence);
+};
 
 export const login = async (email: string, password: string) => {
-  const auth = getAuth()
+  const auth = getAuth();
   try {
-    await signInWithEmailAndPassword(auth, email, password)
-    Notify.create('Login successful!')
-    return true
-  } catch(err) {
-    Notify.create(`Failed to log in! ${err}`)
-    return false
+    await signInWithEmailAndPassword(auth, email, password);
+    Notify.create('Login successful!');
+    return true;
+  } catch (err) {
+    Notify.create(`Failed to log in! ${err}`);
+    return false;
   }
-}
+};
 
 export const register = async (email: string, password: string) => {
-  const auth = getAuth()
+  const auth = getAuth();
   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
     setDoc(doc(collection(firestore, 'users'), userCredential.user.uid), {
-      registrationDate: new Date()
-    })
-    Notify.create('Almost there!')
-    return true
-  } catch(err) {
-    Notify.create(`An error occured: ${err}`)
-    return false
+      registrationDate: serverTimestamp(),
+    });
+    Notify.create('Almost there!');
+    return true;
+  } catch (err) {
+    Notify.create(`An error occured: ${err}`);
+    return false;
   }
-}
+};
 
 export const getUser = () => {
-
   const auth = getAuth();
 
   try {
     // For easy access to all user info, store in object each one
-    const userinfo: {email?: string, name?: string} = {};
+    const userinfo: { email?: string; name?: string } = {};
     const user = auth.currentUser;
 
     if (user !== null) {
@@ -75,16 +94,13 @@ export const getUser = () => {
       userinfo.email = email;
       if (email !== null) {
         userinfo.name = email.substr(0, email.indexOf('@'));
-      } 
+      }
     }
-    return userinfo
+    return userinfo;
+  } catch (err) {
+    Notify.create(`An error occured: ${err}`);
+    return { email: null, name: null };
+  }
+};
 
-   } catch(err) {
-      Notify.create(`An error occured: ${err}`)
-      return { email: null, name: null }
-    }
-} 
-   
-
-
-console.log('Firebase app initialized!')
+console.log('Firebase app initialized!');
