4aa81548f4686f7fc1ef6ca8d5c06d88f0141c76
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-14 20:31:33 +0800

    :construction: LanguageSelector component

---
+++ src/components/LangSelector.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4 shadow-xl">
+    <div class="text-h6 font-black">Language</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LangSelector'
+})
+</script>
