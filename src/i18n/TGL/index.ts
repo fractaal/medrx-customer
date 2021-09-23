808c722a3be0404baec4fff2efb581040457fa3d
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-23 17:00:57 +0800

    :art: Make keys camelCase

--- src/i18n/TGL/index.ts
+++ src/i18n/TGL/index.ts
@@ -4,17 +4,17 @@
 // TODO: Make casing between keys consistent (all should be in camelCase)
 
 export default {
-  Failed: 'Bigo ang pagkilos',
-  Success: 'Matagumpay ang pagkilos',
-  Welcome: 'Maligayang pagdating sa MedRx!',
-  Email: 'Email',
-  Password: 'Password',
-  Login: 'LOG IN',
-  Signup: 'SIGN-UP',
-  Settings: 'WOAH MAGIC',
-  Hello: 'Kamusta',
-  If: 'Kung gusto mong mabuhay...',
-  Buy: 'Kainin mo ang pwet ni Pedro',
-  Or: 'o mamatay ka.',
-  Language: 'Wika'
+  failed: 'Bigo ang pagkilos',
+  success: 'Matagumpay ang pagkilos',
+  welcome: 'Maligayang pagdating sa MedRx!',
+  email: 'Email',
+  password: 'Password',
+  login: 'LOG IN',
+  signUp: 'Mag Sign-up',
+  settings: 'WOAH MAGIC',
+  hello: 'Kamusta',
+  if: 'Kung gusto mong mabuhay...',
+  buy: 'Kainin mo ang pwet ni Pedro',
+  or: 'o mamatay ka.',
+  language: 'Wika',
 };
--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -3,19 +3,18 @@
 
 // TODO: Make casing between keys consistent (all should be in camelCase)
 
-
 export default {
-  Failed: 'Failed to login!',
-  Success: 'Login successful!',
-  Welcome: 'Welcome to MedRx!',
-  Email: 'Email',
-  Password: 'Password',
-  Login: 'LOG IN',
-  Signup: 'SIGN-UP',
-  Settings: 'Settings',
-  Hello: 'Hello',
-  If: 'If you want to live...',
-  Buy: 'Buy these essential medical products',
-  Or: 'Or die',
-  Language: 'Language'
+  failed: 'Failed to login!',
+  success: 'Login successful!',
+  welcome: 'Welcome to MedRx!',
+  email: 'Email',
+  password: 'Password',
+  login: 'LOG IN',
+  signUp: 'Sign Up',
+  settings: 'Settings',
+  hello: 'Hello',
+  if: 'If you want to live...',
+  buy: 'Buy these essential medical products',
+  or: 'Or die',
+  language: 'Language',
 };
