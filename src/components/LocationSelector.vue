e082281523fa075d4bd48b1b85262fc9b3037d6c
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-24 03:49:22 +0800

    :art: Formatting

--- src/components/LocationSelector.vue
+++ src/components/LocationSelector.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="gap-4 px-10 grid-cols-2 grid">
-    <q-select
-      dense
-      outlined
-      label="Region"
-      :options="regions"
-      v-model="selectedRegion"
-      class="w-full"
-    />
+    <q-select dense outlined label="Region" :options="regions" v-model="selectedRegion" class="w-full" />
     <q-select
       dense
       outlined
@@ -21,10 +14,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, computed, watch } from 'vue'
-import { firestore } from 'src/api/firebase'
-import { useQuasar } from 'quasar'
-import { getDoc, doc, collection, DocumentData } from 'firebase/firestore'
+import { defineComponent, ref, onMounted, computed, watch } from 'vue';
+import { firestore } from 'src/api/firebase';
+import { useQuasar } from 'quasar';
+import { getDoc, doc, collection, DocumentData } from 'firebase/firestore';
 
 export default defineComponent({
   name: 'LocationSelector',
@@ -36,20 +29,20 @@ export default defineComponent({
 
     const data = ref<DocumentData>({});
     const regions = ref<string[]>([]);
-    const cities = computed<string[]>(() => data.value[selectedRegion.value] ?? [])
+    const cities = computed<string[]>(() => data.value[selectedRegion.value] ?? []);
 
     onMounted(async () => {
       quasar.loading.show();
-      data.value = (await getDoc(doc(collection(firestore, 'service'), 'regions'))).data()!
+      data.value = (await getDoc(doc(collection(firestore, 'service'), 'regions'))).data() as DocumentData;
 
-      regions.value = Object.keys(data.value).map(region => region)
+      regions.value = Object.keys(data.value).map((region) => region);
       quasar.loading.hide();
-    })
+    });
 
     watch([selectedRegion, selectedCity], () => {
-      console.log("emitting!")
-      emit('input', { region: selectedRegion.value, city: selectedCity.value })
-    })
+      console.log('emitting!');
+      emit('input', { region: selectedRegion.value, city: selectedCity.value });
+    });
 
     return {
       // Field values
@@ -59,7 +52,7 @@ export default defineComponent({
       // Plain values
       regions,
       cities,
-    }
-  }
-})
+    };
+  },
+});
 </script>
