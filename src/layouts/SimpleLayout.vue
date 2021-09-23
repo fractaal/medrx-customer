6ed7aa624d2291f06d728c90f42dc5cbb2455ec4
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-24 03:55:50 +0800

    :lipstick: Cityscapes

--- src/layouts/SimpleLayout.vue
+++ src/layouts/SimpleLayout.vue
@@ -1,12 +1,8 @@
 <template>
   <q-layout view="hHh lpR fFf">
     <q-page-container>
-      <transition
-        enter-active-class="animated fadeIn"
-        leave-active-class="animated fadeOut"
-        appear
-        :duration="1000"
-      >
+      <img src="~assets/cityscape.svg" class="fixed bottom-4 opacity-10" />
+      <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" appear :duration="1000">
         <router-view class="mx-auto w-full md:w-3/4 lg:w-3/5" />
       </transition>
     </q-page-container>
@@ -15,12 +11,8 @@
 
 <script>
 export default {
-  name: 'SimpleLayout'
-}
+  name: 'SimpleLayout',
+};
 </script>
 
-<style scoped>
-</style>
-
-
-      
+<style scoped></style>
