eef197cbc979de0632f70f704657d0b72ab3d3de
Ben Jude <benedict.jude.rocat@gmail.com>
2021-09-24 15:13:38 +0800

    :fire: Remove goBackToPreviousStatusBarStyle

--- src/api/mobile.ts
+++ src/api/mobile.ts
@@ -1,4 +1,4 @@
-const api = {} as Record<string, any>;
+let api = {} as Record<string, any>;
 
 const statusBarHistory: { color: string; dark: boolean }[] = [];
 
@@ -6,19 +6,14 @@ if (process.env.MODE === 'capacitor') {
   import('@capacitor/core').then(({ Plugins, StatusBarStyle }) => {
     const { StatusBar, PushNotifications } = Plugins;
 
-    api.setStatusBarColor = (color: string, dark = false) => {
+    api.setStatusBarColor = (color = '#ffffff', dark = false) => {
+      console.log(`Setting status bar to ${color} isDark:${dark}`);
       StatusBar.setBackgroundColor({ color });
       StatusBar.setStyle({ style: dark ? StatusBarStyle.Dark : StatusBarStyle.Light });
       statusBarHistory.push({ color, dark });
     };
 
-    api.goBackToPreviousStatusBarStyle = () => {
-      const { color, dark } = statusBarHistory.pop()!;
-      StatusBar.setBackgroundColor({ color: color ?? '#ffffff' });
-      StatusBar.setStyle({ style: dark ?? false ? StatusBarStyle.Dark : StatusBarStyle.Light });
-    };
-
-    api.setStatusBarColor('#ffffff');
+    api.setStatusBarColor();
 
     // Request permission to use push notifications
     // iOS will prompt user and return if they granted permission or not
@@ -49,6 +44,19 @@ if (process.env.MODE === 'capacitor') {
     PushNotifications.addListener('pushNotificationActionPerformed', (notification: any) => {
       console.log('Push action performed: ' + JSON.stringify(notification));
     });
+  });
+} else {
+  // Proxy that redirects method calls to no-op if the platform is not Capacitor.
+  api = new Proxy(api, {
+    get(_, propKey: string) {
+      return (...args: any[]) => {
+        console.log(
+          `Intercepted method call to ${propKey} with args ${JSON.stringify(
+            args
+          )} - Method called is now NO-OP (NOT CAPACITOR APP!)`
+        );
+      };
+    },
   });
 }
 
