174e010ef2336bb38faa32ed8fb086552ffa6a6a
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-04 01:12:14 +0800

    :recycle: Format code and some firebase changes
    initialize firebase and then intialize axios with the appropriate server address dictated by Firebase Remote Config.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,38 @@
 <template>
   <router-view v-slot="{ Component }">
-    <transition
-      enter-active-class="animated fadeIn"
-      leave-active-class="animated fadeOut"
-      :duration="150"
-    >
+    <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" :duration="150">
       <component :is="Component" />
     </transition>
   </router-view>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
-import { getAuth } from 'firebase/auth'
-import { init } from 'src/api/firebase'
+import { defineComponent } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import { getAuth } from 'firebase/auth';
+import { init as initFirebase } from 'src/api/firebase';
+import { init as initAxios } from 'src/boot/axios';
 
 if (process.env.MODE === 'capacitor') {
-  import('src/api/mobile')
+  import('src/api/mobile');
 }
 
 export default defineComponent({
   name: 'App',
   setup() {
-    init()
+    initFirebase().then(() => initAxios());
 
-    const router = useRouter()
-    const route = useRoute()
+    const router = useRouter();
+    const route = useRoute();
 
-    getAuth().onAuthStateChanged(user => {
-      if (route.path !== '/') return
+    getAuth().onAuthStateChanged((user) => {
+      if (route.path !== '/') return;
 
       if (user) {
-        router.push('/home')
+        router.push('/home');
       } else {
-        router.push('/login')
+        router.push('/login');
       }
-
-    })
-  }
-})
+    });
+  },
+});
 </script>
