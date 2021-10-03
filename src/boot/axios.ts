6a7122a7209b37f80693d8d60fc65b23e2f957a6
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-04 00:42:29 +0800

    :alien: axios should contact API at address set on Firebase remote config

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
+import { getRemoteConfig, getString } from 'firebase/remote-config';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
@@ -13,7 +14,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const api = axios.create({ baseURL: getString(getRemoteConfig(), 'serverAddress') });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
