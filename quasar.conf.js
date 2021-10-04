29b5395c87fbffb3d84fabc8347d7c69b61f2f9e
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-04 23:37:01 +0800

    :wrench: Add initialize boot file

--- quasar.conf.js
+++ quasar.conf.js
@@ -31,6 +31,7 @@ module.exports = configure(function (ctx) {
     boot: [
       'i18n',
       'axios',
+      'initialize'
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
