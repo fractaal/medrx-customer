3e06df853df6a07fe540be7886f47e56319e0cf4
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-05 15:12:13 +0800

    :card_file_box: Add Product model

---
+++ src/models/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  vendorId: number;
+  name: string;
+  description: string;
+  price: number;
+  metadata?: any;
+  isPrescriptionRequired: boolean;
+  updatedAt?: any;
+  createdAt: Date;
+}
