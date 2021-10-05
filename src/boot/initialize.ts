d247cbe1ed8b60cf97b988e37f984168a2918d15
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-06 01:55:52 +0800

    :loud_sound: Add some logging

--- src/boot/initialize.ts
+++ src/boot/initialize.ts
@@ -3,6 +3,9 @@ import { init as initFirebase } from 'src/api/firebase';
 import { init as initAxios } from 'src/boot/axios';
 
 export default boot(async () => {
+  console.log('Initializing Firebase...');
   await initFirebase();
+  console.log('Done! Initializing Axios...');
   await initAxios();
+  console.log('Done!');
 });
