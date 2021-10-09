a79b1918689523b2122aacee6608d066797c2dab
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-09 16:59:41 +0800

    :recycle: Move cart logic to cart.ts | add TODOO

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -127,6 +127,7 @@ export const getUser = async () => {
     const user = auth.currentUser;
 
     if (user !== null) {
+      // TODO: This can be simplified.
       userinfo.email = user.email!;
       userinfo.firstName = (await getDoc(doc(collection(firestore, 'users'), user.uid))).data()!.firstName;
       userinfo.middleName = (await getDoc(doc(collection(firestore, 'users'), user.uid))).data()!.middleName;
@@ -143,45 +144,6 @@ export const getUser = async () => {
   } catch (err) {
     Notify.create(`An error occured: ${(err as Error).message}`);
     // return { email: null, name: null };
-  }
-};
-
-
-export const updateCart = async (name: string, quantity: number, price: number) => {
-  console.log('Added to cart', name, quantity, price);
-  const auth = getAuth();
-  const uid = auth.currentUser?.uid;
-
-  try {
-
-    const ref = doc(collection(firestore, 'users'), uid)
-    await setDoc(ref, {
-      cart: [{ name: name, quantity: quantity, price: price }]
-    }, { merge: true });
-
-    return true;
-  } catch (err) {
-    Notify.create(`An error occured: ${(err as Error).message}`);
-    return false;
-  }
-}
-
-export const getCart = async () => {
-  const auth = getAuth();
-
-  try {
-    // For easy access to all user info, store in object each one
-
-    const user = auth.currentUser;
-
-    if (user !== null) {
-      const cart = (await getDoc(doc(collection(firestore, 'users'), user.uid))).data()!.cart;
-      return cart;
-    }
-    return null
-  } catch (err) {
-    Notify.create(`An error occured: ${(err as Error).message}`);
-
   }
 };
 
