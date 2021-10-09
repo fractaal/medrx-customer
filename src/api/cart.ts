a989c3ac95205edf16c81f5967e67f65762c3608
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-09 17:51:35 +0800

    :recycle: Fix various stuff, add ref for cart quantity

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,18 +1,34 @@
-export const updateCart = async (name: string, quantity: number, price: number) => {
-  console.log('Added to cart', name, quantity, price);
-  const auth = getAuth();
-  const uid = auth.currentUser?.uid;
+import { Notify } from 'quasar';
+import { ref } from 'vue';
+import { getDoc, doc, updateDoc, onSnapshot } from 'firebase/firestore';
+import { getAuth } from '@firebase/auth';
+import { firestore } from './firebase';
+import { CartItem } from 'src/models/CartItem';
+import { SrvRecord } from 'dns';
 
+const auth = getAuth();
+const uid = auth.currentUser?.uid;
+
+export const itemsInCart = ref(0);
+
+export const addToCart = async (
+  productId: number,
+  productName: string,
+  productQuantity: number,
+  productPrice: number
+) => {
   try {
-    const ref = doc(collection(firestore, 'users'), uid);
-    await setDoc(
-      ref,
-      {
-        cart: [{ name: name, quantity: quantity, price: price }],
-      },
-      { merge: true }
-    );
-
+    // const ref = doc(firestore, 'users', uid, 'cart');
+    const ref = doc(firestore, `users/${uid}`);
+    const existingQuantity: number = (await getDoc(ref)).get(`cart.${productId}.productQuantity`) ?? 0;
+    await updateDoc(ref, `cart.${productId}`, {
+      productId,
+      productName,
+      productPrice,
+      productQuantity: existingQuantity + productQuantity,
+    });
+    console.log('Added to cart', productName, productQuantity, productPrice);
+    Notify.create({ message: `${productName} has been added to the cart!`, type: 'positive' });
     return true;
   } catch (err) {
     Notify.create(`An error occured: ${(err as Error).message}`);
@@ -20,20 +36,24 @@ export const updateCart = async (name: string, quantity: number, price: number) 
   }
 };
 
-export const getCart = async () => {
-  const auth = getAuth();
-
+export const getCart = async (): Promise<Record<string, CartItem>> => {
   try {
-    // For easy access to all user info, store in object each one
-
     const user = auth.currentUser;
-
     if (user !== null) {
-      const cart = (await getDoc(doc(collection(firestore, 'users'), user.uid))).data()!.cart;
-      return cart;
+      const cart = (await getDoc(doc(firestore, `users/${uid}`))).data()!.cart;
+      return cart ?? {};
     }
-    return null;
+    return {};
   } catch (err) {
     Notify.create(`An error occured: ${(err as Error).message}`);
+    return {};
   }
 };
+
+onSnapshot(doc(firestore, `users/${uid}`), (doc) => {
+  const data = doc.data()!;
+  const cart = data.cart as Record<string, CartItem>;
+  let total = 0;
+  for (const productId in cart) total += cart[productId].productQuantity;
+  itemsInCart.value = total;
+});
