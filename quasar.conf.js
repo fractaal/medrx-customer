70a767b264e5bcbc9b3a8fb5406b352cf5fbb967
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-13 02:47:32 +0800

    :wrench: Add Dialog plugin

--- quasar.conf.js
+++ quasar.conf.js
@@ -104,7 +104,8 @@ module.exports = configure(function (ctx) {
       plugins: [
         'Notify',
         'LocalStorage',
-        'Loading'
+        'Loading',
+        'Dialog'
       ]
     },
 
