6a4c307464d3399c66700d481989965b82c8f2f3
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-13 02:13:33 +0800

    :dizzy: Tweak animations!

--- src/pages/Splash.vue
+++ src/pages/Splash.vue
@@ -1,12 +1,12 @@
 <template>
-  <div style="width: 100vw; height: 100vh;" class="flex justify-center content-center">
+  <div style="width: 100vw; height: 100vh" class="flex justify-center content-center">
     <div>
       <video
         ref="video"
         playsinline
         loop
         autoplay
-        style="width: 500px;"
+        style="width: 500px"
         :style="style"
         @loadeddata="show"
         class="mx-auto"
@@ -20,36 +20,33 @@
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue'
-import { onBeforeRouteLeave } from 'vue-router'
+import { defineComponent, ref, onMounted } from 'vue';
+import { onBeforeRouteLeave } from 'vue-router';
 export default defineComponent({
   name: 'Splash',
   setup() {
-    const style = ref('display: none; z-index: 10;')
-    const animation = ref('')
-    const video = ref(null)
+    const style = ref('display: none; z-index: 10;');
+    const animation = ref('');
+    const video = ref(null);
 
     onMounted(() => {
       try {
-        video.value.play()
-      } catch (err) { }
-      animation.value = 'splash-logo-enter'
-    })
+        video.value.play();
+      } catch (err) {}
+      animation.value = 'splash-logo-enter';
+    });
 
-    onBeforeRouteLeave(async () => {
-      await new Promise(r => setTimeout(r, 900))
-      animation.value = 'splash-logo-leave'
-      await new Promise(r => setTimeout(r, 690))
-      await new Promise(r => setTimeout(r, 10))
-      return true
-    })
+    onBeforeRouteLeave(() => {
+      animation.value = 'splash-logo-leave';
+      return true;
+    });
 
     return {
       style,
       animation,
       video,
-      show: () => style.value = 'display: block; z-index: 10;'
-    }
+      show: () => (style.value = 'display: block; z-index: 10;'),
+    };
   },
-})
+});
 </script>
