a661122122c5665b82201c288a92fc3316593ff2
lcsantos11395 <lcsantos11395@gmail.com>
2021-10-19 00:01:15 +0800

    :construction: Implementation of new productitem component

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,18 +1,31 @@
 <template>
   <q-page>
     <q-input :debounce="500" v-model="searchTerm"></q-input>
-    <div>
-      <p v-for="product in searchResults" :key="product.id">{{ product }}</p>
-    </div>
+      <div>
+        <q-list bordered padding>
+            <product-item
+                v-for="product in searchResults"
+                clickable
+                @click="$router.push(`/product/${product.id}`)"
+                :photo-url="product.photoUrl"
+                :key="product.id"
+                :itemname="product.name"
+                :description="product.description"
+                :price="product.price"
+              />
+        </q-list>
+      </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { searchTerm, searchResults } from 'src/api/search';
+import ProductItem from 'src/components/ProductItem.vue';
 
 export default defineComponent({
   name: 'Search',
+  components: { ProductItem },
   setup() {
     return {
       searchTerm,
