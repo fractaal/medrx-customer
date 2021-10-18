5d29e99d9d2ff2f14968c89742e968db7b1f6155
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-19 00:52:22 +0800

    :bug: Do not perform request if searchTerm is empty

--- src/api/search.ts
+++ src/api/search.ts
@@ -5,6 +5,7 @@ import { Product } from 'src/models/Product';
 
 export const searchResults = ref<Product[]>([]);
 export const searchTerm = ref('');
+export const isLoading = ref(false);
 
 watch(searchTerm, async (newSearchTerm) => {
   searchResults.value = [];
@@ -12,7 +13,12 @@ watch(searchTerm, async (newSearchTerm) => {
 });
 
 export const search = async (searchTerm: string) => {
+  if (searchTerm == '') {
+    return [];
+  }
   console.log('searching for', searchTerm);
+  isLoading.value = true;
   const res = await api.post<ResponseData<Record<string, any>>>('/search', { searchTerm });
+  isLoading.value = false;
   return res.data.data.results;
 };
