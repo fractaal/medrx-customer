5ae0519ccd1b6aeb83188ad50b45f52ff9afd41e
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-19 00:51:58 +0800

    :lipstick: Update UI design

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,20 +1,32 @@
 <template>
-  <q-item>
-    <q-item-section top thumbnail class="q-ml-none">
-
-      <q-avatar >
-        <img :src="photoUrl || placeholderImageUrl" ratio="1" class="h-30" />
+  <!-- <q-item class="rounded-xl">
+    <q-item-section top>
+      <q-avatar>
+        <img :src="photoUrl || placeholderImageUrl" ratio="1" />
       </q-avatar>
-
     </q-item-section>
-    
+
     <q-item-section>
       <q-item-label>{{ itemname }}</q-item-label>
       <q-item-label caption>{{ description }}</q-item-label>
     </q-item-section>
 
-    <q-item-section side class="font-semibold">
+    <q-item-section class="font-semibold">
       <q-item-label caption>{{ price }}</q-item-label>
+    </q-item-section>
+  </q-item> -->
+  <q-item clickable v-ripple class="rounded-xl shadow-lg py-4 bg-white">
+    <q-item-section avatar>
+      <q-img :src="photoUrl || placeholderImageUrl" ratio="1" class="rounded-md h-16 w-16" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label class="font-black">{{ itemname }}</q-item-label>
+      <q-item-label>{{ description }}</q-item-label>
+    </q-item-section>
+
+    <q-item-section side class="font-semibold">
+      <q-item-label class="font-black">{{ price }} PHP</q-item-label>
     </q-item-section>
   </q-item>
 </template>
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,26 +1,34 @@
 <template>
   <q-page>
-    <q-input :debounce="500" v-model="searchTerm"></q-input>
-      <div>
-        <q-list bordered padding>
-            <product-item
-                v-for="product in searchResults"
-                clickable
-                @click="$router.push(`/product/${product.id}`)"
-                :photo-url="product.photoUrl"
-                :key="product.id"
-                :itemname="product.name"
-                :description="product.description"
-                :price="product.price"
-              />
-        </q-list>
-      </div>
+    <q-input
+      class="mt-4 px-4"
+      placeholder="Search for products..."
+      rounded
+      outlined
+      :debounce="500"
+      v-model="searchTerm"
+    ></q-input>
+    <div class="mt-4">
+      <q-spinner v-if="isLoading" size="xl" class="block mx-auto" />
+      <q-list padding class="space-y-2" v-else>
+        <product-item
+          v-for="product in searchResults"
+          clickable
+          @click="$router.push(`/product/${product.id}`)"
+          :photo-url="product.photoUrl"
+          :key="product.id"
+          :itemname="product.name"
+          :description="product.description"
+          :price="product.price"
+        />
+      </q-list>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { searchTerm, searchResults } from 'src/api/search';
+import { searchTerm, searchResults, isLoading } from 'src/api/search';
 import ProductItem from 'src/components/ProductItem.vue';
 
 export default defineComponent({
@@ -30,6 +38,7 @@ export default defineComponent({
     return {
       searchTerm,
       searchResults,
+      isLoading,
     };
   },
 });
