a833cde4719bbb25bfb3d8e9dfeb6931a9af1d13
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-21 22:13:12 +0800

    :card_file_box: Update Product model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,12 +1,13 @@
 export interface Product {
   id: string;
-  vendorId: number;
+  vendorId: string;
   name: string;
   description: string;
   price: number;
   metadata?: any;
   isPrescriptionRequired: boolean;
-  updatedAt?: any;
-  createdAt: Date;
+  photoUrl?: any;
+  dateUpdated?: any;
+  dateCreated: Date;
   [x: string]: any;
 }
