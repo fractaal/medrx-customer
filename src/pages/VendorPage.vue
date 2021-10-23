aa69830f565db1385d435fce87bd8f0b4599c7c0
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-23 11:49:29 +0800

    :construction: Rework

--- src/pages/VendorPage.vue
+++ src/pages/VendorPage.vue
@@ -3,38 +3,14 @@
     <q-spinner class="w-1/2 mx-auto" v-if="isLoading" size="96px" />
     <div class="flex mt-16" v-else>
       <q-img
-        :src="product?.photoUrl || placeholderImageUrl"
+        :src="vendorData?.vendor.photoUrl || placeholderImageUrl"
         ratio="1"
         class="w-full md:w-1/2 rounded-none md:rounded-xl shadow-md"
       ></q-img>
       <div class="mt-4 md:mt-0 divide-y divide-gray-300 px-8 space-y-4 w-full md:w-1/2">
         <div>
-          <p class="text-h6 font-black">{{ product?.name }}</p>
-          <div
-            v-ripple
-            class="relative flex -mt-3 -mx-2 p-2 rounded-sm"
-            @click="$router.push(`/vendor/${product?.vendorId}`)"
-          >
-            <p class="mr-1">Sold by</p>
-            <p class="font-black">{{ product?.vendorName.toUpperCase() }}</p>
-          </div>
-          <div>{{ product?.description }}</div>
-          <div class="flex content-center justify-end">
-            <div class="text-h6 font-black p-0 m-0">{{ product?.price }}</div>
-            <div class="text-h6 p-0 m-0">PHP</div>
-          </div>
-        </div>
-        <div class="pt-4">
-          <div class="grid grid-cols-2 gap-2">
-            <q-input v-model.number="quantity" type="number" min="1" outlined dense maxlength="12" label="Quantity" />
-            <q-btn
-              class="px-8 bg-gradient-to-tr from-medrx to-green-200"
-              text-color="white"
-              flat
-              @click="addToCart(product?.id ?? '', product?.name ?? '', quantity, product?.price ?? 0)"
-              >Add to Cart</q-btn
-            >
-          </div>
+          <p class="text-h6 font-black">{{ vendorData?.vendor.name }}</p>
+          <div>{{ vendorData?.vendor.content }}</div>
         </div>
       </div>
     </div>
@@ -54,17 +30,17 @@ export default defineComponent({
   setup() {
     const router = useRouter();
     const vendorId = router.currentRoute.value.params.id;
-    const vendor = ref<Data | null>(null);
+    const vendorData = ref<Data | null>(null);
 
     onMounted(async () => {
-      vendor.value = await getVendor(vendorId as string);
+      vendorData.value = await getVendor(vendorId as string);
     });
 
     return {
       placeholderImageUrl,
       addToCart,
       isLoading,
-      vendor,
+      vendorData,
     };
   },
 });
