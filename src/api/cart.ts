8fb7923bcf20cc4c0e56eb61e20ed7bc6cc73b28
lcsantos11395 <lcsantos11395@gmail.com>
2021-10-24 21:52:55 +0800

    :beers: added firebase adjustments

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,6 +1,6 @@
 import { Notify, Dialog } from 'quasar';
 import { ref } from 'vue';
-import { getDoc, doc, updateDoc, onSnapshot } from 'firebase/firestore';
+import { getDoc, doc, updateDoc, onSnapshot, deleteField } from 'firebase/firestore';
 import { getAuth } from '@firebase/auth';
 import { firestore } from './firebase';
 import { CartItem } from 'src/models/CartItem';
@@ -9,6 +9,7 @@ const auth = getAuth();
 const uid = auth.currentUser?.uid;
 
 export const itemsInCart = ref(0);
+export const cart = ref<Record<string, CartItem>>({});
 
 export const addToCart = (productId: string, productName: string, productQuantity: number, productPrice: number) => {
   try {
@@ -53,11 +54,13 @@ export const addToCart = (productId: string, productName: string, productQuantit
   }
 };
 
+//Can be combined with getAmount(). To be refactored
 export const getTotal = async () => {
   try {
     const user = auth.currentUser;
     if (user !== null) {
       const subTotal = (await getDoc(doc(firestore, `users/${uid}`))).data()!.subTotal;
+
       return subTotal;
     }
     return 0;
@@ -67,45 +70,100 @@ export const getTotal = async () => {
   }
 };
 
-// export const updateCart = async (productId: string, productName: string, productQuantity: number, productPrice: number) => {
-//   try {
-//     const user = auth.currentUser;
-//     const uid = auth.currentUser?.uid;
-
-//     if (user !== null) {
-//       const ref = doc(firestore, `users/${uid}`);
-//       const currentPrice: number = (await getDoc(ref)).get(`cart.${productId}.productPrice`) ?? 0;
-
-//       await updateDoc(ref, `cart.${productId}`, {
-//         productId,
-//         productName,
-//         productPrice: productPrice,
-//         productQuantity: latestQuantity,
-//       });
-//     }
-//   }
-// }
-
-export const getCart = async (): Promise<Record<string, CartItem>> => {
+export const getAmount = async (productId: string) => {
   try {
     const user = auth.currentUser;
     if (user !== null) {
-      const cart = (await getDoc(doc(firestore, `users/${uid}`))).data()!.cart;
-      return cart ?? {};
+      const ref = doc(firestore, `users/${uid}`);
+      const amount: number = (await getDoc(ref)).get(`cart.${productId}.amount`) ?? 0;
+
+      return amount;
     }
-    return {};
+    return 0;
   } catch (err) {
     Notify.create(`An error occured: ${(err as Error).message}`);
-    return {};
+    return 0;
   }
 };
+
+export const removeProduct = async (productId: string) => {
+  try {
+    const user = auth.currentUser;
+    const uid = auth.currentUser?.uid;
+
+    if (user !== null) {
+      const ref = doc(firestore, `users/${uid}`);
+      const currentAmount: number = (await getDoc(ref)).get(`cart.${productId}.amount`) ?? 0;
+      await updateDoc(ref, { [`cart.${productId}`]: deleteField() });
+      const currentSubTotal = (await getDoc(doc(firestore, `users/${uid}`))).data()!.subTotal;
+      const newSubTotal = currentSubTotal - currentAmount;
+
+      await updateDoc(ref, {
+        subTotal: newSubTotal,
+      });
+    }
+
+  } catch (err) {
+    Notify.create(`An error occured: ${(err as Error).message}`);
+    return false;
+  }
+}
+
+
+export const updateCart = async (productId: string, productName: string, productQuantity: number, productPrice: number) => {
+  try {
+    const user = auth.currentUser;
+    const uid = auth.currentUser?.uid;
+
+    if (user !== null) {
+      const ref = doc(firestore, `users/${uid}`);
+      const amount = productQuantity * productPrice;
+      const currentAmount: number = (await getDoc(ref)).get(`cart.${productId}.amount`) ?? 0;
+      const currentSubTotal = (await getDoc(doc(firestore, `users/${uid}`))).data()!.subTotal;
+      const difference: number = currentAmount - amount;
+      const latestSubTotal: number = currentSubTotal - difference;
+
+      await updateDoc(ref, `cart.${productId}`, {
+        productId,
+        productName,
+        productPrice,
+        productQuantity,
+        amount: amount
+      });
+
+      await updateDoc(ref, {
+        subTotal: latestSubTotal,
+      });
+    }
+  } catch (err) {
+    Notify.create(`An error occured: ${(err as Error).message}`);
+    return false;
+  }
+}
+
+// export const getCart = async (): Promise<Record<string, CartItem>> => {
+//   try {
+//     const user = auth.currentUser;
+//     if (user !== null) {
+//       const cart = (await getDoc(doc(firestore, `users/${uid}`))).data()!.cart;
+//       return cart ?? {};
+//     }
+//     return {};
+//   } catch (err) {
+//     Notify.create(`An error occured: ${(err as Error).message}`);
+//     return {};
+//   }
+// };
+
+
 
 
 
 onSnapshot(doc(firestore, `users/${uid}`), (doc) => {
   const data = doc.data()!;
-  const cart = data.cart as Record<string, CartItem>;
+  const _cart = data.cart as Record<string, CartItem>;
   let total = 0;
-  for (const productId in cart) total += cart[productId].productQuantity;
+  for (const productId in _cart) total += _cart[productId].productQuantity;
   itemsInCart.value = total;
+  cart.value = _cart;
 });
