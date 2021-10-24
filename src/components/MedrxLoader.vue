b94fa8d9430c3eead204504f6e28f556c0cb3732
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-24 21:18:23 +0800

    :sparkles: Add medrx loader component

---
+++ src/components/MedrxLoader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex content-center justify-center">
+    <q-spinner size="72px" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MedRxLoader',
+});
+</script>
