8df54ad483e46becad62afa3636475f356452a05
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-27 14:56:30 +0800

    :coffin: Remove comments

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <q-page class="bg-body">
-    <!-- <horizontal-scroller> -->
-    <!-- <q-spinner class="block mx-auto" size="xl" v-if="storefrontIsLoading" /> -->
     <medrx-loader v-if="storefrontIsLoading" style="height: calc(100vh - 150px)" />
     <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mx-4">
       <product-card
@@ -15,19 +13,6 @@
         :price="item.price"
       />
     </div>
-    <!-- </horizontal-scroller> -->
-    <!-- <q-dialog v-model="confirm" position="bottom">
-      <q-card>
-        <q-list>
-          <q-card-actions align="around">
-            <q-btn dense class="glossy ml-4" round color="primary" icon="remove" @click="quantityChecker()" />
-            <div class="p-3">{{ quantity }}</div>
-            <q-btn dense class="glossy" round color="primary" icon="add" @click="quantity++" />
-            <q-btn flat label="Confirm" color="primary" style="width: 100px" v-close-popup />
-          </q-card-actions>
-        </q-list>
-      </q-card>
-    </q-dialog> -->
   </q-page>
 </template>
 
