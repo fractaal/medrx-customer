d23bd880ff1db1efa2606f8644e622c3f70df943
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-28 20:23:28 +0800

    :label: Add custom user claims to global type defs

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,2 +1,11 @@
-declare module '@capacitor/core'
-declare module '@capacitor/status-bar'
+import '@firebase/auth';
+
+declare module '@capacitor/core';
+declare module '@capacitor/status-bar';
+declare module '@firebase/auth' {
+  interface ParsedToken {
+    region: string;
+    city: string;
+    roles: string;
+  }
+}
