028fe5a04de5dc08d6bf6cf1a162289703d127b3
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-29 02:40:13 +0800

    :recycle: Move capacitor definitions to its own file

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,7 +1,5 @@
 import '@firebase/auth';
 
-declare module '@capacitor/core';
-declare module '@capacitor/status-bar';
 declare module '@firebase/auth' {
   interface ParsedToken {
     region: string;
