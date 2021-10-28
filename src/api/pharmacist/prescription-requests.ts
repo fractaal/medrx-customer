da177ea93fa58c5ac0206cc98ec074293a15e81f
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-29 02:41:57 +0800

    :sparkles: new prescription requests module

---
+++ src/api/pharmacist/prescription-requests.ts
@@ -0,0 +1,27 @@
+import { ref } from 'vue';
+import { database } from '../firebase';
+import { token } from '../auth';
+import * as db from 'firebase/database';
+import { PrescriptionRequestStatus } from '../prescription';
+
+interface PrescriptionRequest {
+  status: PrescriptionRequestStatus | undefined;
+  customMessage: string | undefined;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  photoUrl: string;
+}
+
+export const prescriptionRequests = ref<Record<string, PrescriptionRequest>>({});
+
+db.onValue(db.ref(database, `/${token.value?.claims.region}/${token.value?.claims.city}/`), (snapshot) => {
+  const raw: Record<string, { prescriptionRequests: PrescriptionRequest }> = snapshot.val();
+  const transformed: Record<string, PrescriptionRequest> = {};
+
+  Object.keys(raw).map((id) => {
+    transformed[id] = raw[id].prescriptionRequests;
+  });
+
+  prescriptionRequests.value = transformed;
+});
