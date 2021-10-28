2f83ce586016e97e2ca2ce7251fb002fcdf83985
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-29 03:39:00 +0800

    :lipstick: WIP UI

--- src/layouts/PharmacistLayout.vue
+++ src/layouts/PharmacistLayout.vue
@@ -1,17 +1,23 @@
 <template>
   <q-layout>
+    <q-header class="bg-transparent">
+      <q-toolbar class="bg-transparent text-black">
+        <q-btn flat round dense icon="arrow_left" @click="$router.back()" class="q-mr-sm" />
+        <q-toolbar-title>MedRx Pharmacist</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
     <q-page>
       <q-page-container>
-        <div class="flex flex-nowrap transition-all duration-100 p-4 h-screen">
+        <div class="flex flex-nowrap transition-all duration-100 p-4" style="height: calc(100vh - 50px)">
           <div
-            class="h-full overflow-y-auto shadow-xl rounded-md ring-1 ring-medrx transition-all duration-100 mr-2"
-            :class="test ? 'w-1/4' : 'w-0'"
+            class="h-full overflow-y-auto shadow-xl rounded-md ring-medrx transition-all duration-200 mr-2"
+            :class="leftDrawerOpen ? 'w-1/4 ring-2' : 'w-0 ring-0'"
           >
-            <div v-show="test" class="space-y-2 p-4">
+            <div v-show="leftDrawerOpen" class="space-y-2 p-4">
               <transition-group name="list">
                 <div v-for="(request, id) in prescriptionRequests" :key="id">
                   <product-item
-                    :title="request.firstName + ' ' + request.middleName + ' ' + request.lastName"
+                    :itemname="request.firstName + ' ' + request.middleName + ' ' + request.lastName"
                     description="placeholder"
                     :photoUrl="request.photoUrl"
                   >
@@ -21,10 +27,23 @@
             </div>
           </div>
           <div
-            class="h-full shadow-xl rounded-md ring-1 ring-medrx transition-all duration-100"
-            :class="test ? 'w-3/4' : 'w-full'"
+            class="relative h-full shadow-xl rounded-md ring-2 ring-medrx transition-all duration-100"
+            :class="leftDrawerOpen ? 'w-3/4' : 'w-full'"
           >
-            <q-toggle v-model="test" />
+            <span class="flex absolute -left-7 -top-7">
+              <span
+                class="absolute inline-flex h-full w-full rounded-full bg-medrx"
+                :class="pingDrawer ? 'animate-ping' : ''"
+              >
+              </span>
+              <q-btn
+                color="primary"
+                round
+                size="18PX"
+                :icon="leftDrawerOpen ? 'arrow_left' : 'arrow_right'"
+                @click="leftDrawerOpen = !leftDrawerOpen"
+              />
+            </span>
           </div>
         </div>
       </q-page-container>
@@ -35,17 +54,30 @@
 <script lang="ts">
 import ProductItem from 'src/components/ProductItem.vue';
 import { prescriptionRequests } from 'src/api/pharmacist/prescription-requests';
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 
 export default defineComponent({
   components: { ProductItem },
   name: 'PharmacistLayout',
   setup() {
     import('src/api/pharmacist/prescription-requests');
-    const test = ref(true);
+    const leftDrawerOpen = ref(true);
+    const pingDrawer = ref(false);
+
+    watch(prescriptionRequests, () => {
+      if (!leftDrawerOpen.value) {
+        pingDrawer.value = true;
+      }
+    });
+
+    watch(leftDrawerOpen, () => {
+      pingDrawer.value = false;
+    });
+
     return {
-      test,
+      leftDrawerOpen,
       prescriptionRequests,
+      pingDrawer,
     };
   },
 });
