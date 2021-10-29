f260ee80e82f905278ce927e06f8e0b3a2bc9a13
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-29 17:07:50 +0800

    :bug: Fix router-view warnings

--- src/layouts/SimpleLayout.vue
+++ src/layouts/SimpleLayout.vue
@@ -2,9 +2,11 @@
   <q-layout view="hHh lpR fFf">
     <q-page-container>
       <img src="~assets/cityscape.svg" class="fixed bottom-4 opacity-10" />
-      <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" appear :duration="1000">
-        <router-view class="mx-auto w-full md:w-3/4 lg:w-3/5" />
-      </transition>
+      <router-view class="mx-auto w-full md:w-3/4 lg:w-3/5" v-slot="{ Component }">
+        <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" appear :duration="1000">
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </q-page-container>
   </q-layout>
 </template>
