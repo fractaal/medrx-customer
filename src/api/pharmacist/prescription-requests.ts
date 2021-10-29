37b79296c7b0aa726074570e8a26b2db40a5ca9d
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-29 12:25:22 +0800

    :recycle: Got logic from prescription
    Photo URLs should be created here instead of on the user. Never trust the client.

--- src/api/pharmacist/prescription-requests.ts
+++ src/api/pharmacist/prescription-requests.ts
@@ -2,6 +2,7 @@ import { ref } from 'vue';
 import { database } from '../firebase';
 import { token } from '../auth';
 import * as db from 'firebase/database';
+import * as storage from 'firebase/storage';
 import { PrescriptionRequestStatus } from '../prescription';
 
 interface PrescriptionRequest {
@@ -15,13 +16,21 @@ interface PrescriptionRequest {
 
 export const prescriptionRequests = ref<Record<string, PrescriptionRequest>>({});
 
-db.onValue(db.ref(database, `/${token.value?.claims.region}/${token.value?.claims.city}/`), (snapshot) => {
+db.onValue(db.ref(database, `/${token.value?.claims.region}/${token.value?.claims.city}/`), async (snapshot) => {
   const raw: Record<string, { prescriptionRequests: PrescriptionRequest }> = snapshot.val() ?? {};
   const transformed: Record<string, PrescriptionRequest> = {};
 
   Object.keys(raw).map((id) => {
     transformed[id] = raw[id].prescriptionRequests;
   });
+
+  await Promise.all(
+    Object.keys(transformed).map(async (id) => {
+      transformed[id].photoUrl = await storage.getDownloadURL(
+        storage.ref(storage.getStorage(), `/users/${id}/prescription.png`)
+      );
+    })
+  );
 
   prescriptionRequests.value = transformed;
 });
