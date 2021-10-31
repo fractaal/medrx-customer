7d170ffa050e8d6f64d34fcd10e93e30bec0f628
Ben Jude <benedict.jude.rocat@gmail.com>
2021-10-31 14:49:38 +0800

    :bug: Recreate onSnapshot every time auth state changes

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -2,30 +2,40 @@ import { ref } from 'vue';
 import { doc, onSnapshot } from 'firebase/firestore';
 import { getAuth } from '@firebase/auth';
 import { firestore } from './firebase';
+import { Unsubscribe } from '@firebase/util';
 
 const auth = getAuth();
-const uid = auth.currentUser?.uid;
-const user = auth.currentUser;
 
-export const firstName = ref('')
-export const middleName = ref('')
-export const lastName = ref('')
-export const phoneNumber = ref('')
-export const address = ref('')
-export const email = ref('')
-export const region = ref('')
-export const city = ref('')
+let unsubscribeUserChange: Unsubscribe;
 
-onSnapshot(doc(firestore, `users/${uid}`), (doc) => {
-  const data = doc.data();
-  firstName.value = data!.firstName
-  middleName.value = data!.middleName
-  lastName.value = data!.lastName
-  address.value = data!.address
-  region.value = data!.region
-  city.value = data!.city
-  email.value = data!.email
-  if (user!.phoneNumber) {
-    phoneNumber.value = user!.phoneNumber;
+export const firstName = ref('');
+export const middleName = ref('');
+export const lastName = ref('');
+export const phoneNumber = ref('');
+export const address = ref('');
+export const email = ref('');
+export const region = ref('');
+export const city = ref('');
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    const uid = user?.uid;
+    unsubscribeUserChange = onSnapshot(doc(firestore, `users/${uid}`), (doc) => {
+      const data = doc.data();
+      console.log('user data changed!', data);
+      firstName.value = data!.firstName;
+      middleName.value = data!.middleName;
+      lastName.value = data!.lastName;
+      address.value = data!.address;
+      region.value = data!.region;
+      city.value = data!.city;
+      email.value = data!.email;
+      if (user.phoneNumber) {
+        phoneNumber.value = user.phoneNumber;
+      }
+    });
+  } else {
+    // There is no more user -- the onSnapshot callback should be unsubscribed.
+    unsubscribeUserChange();
   }
-})
+});
