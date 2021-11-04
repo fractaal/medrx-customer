e00030ed75a02f01e74d7be6e6b5025345cec2cc
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-04 20:48:36 +0800

    :sparkles: Empty placeholder UI element

---
+++ src/components/EmptyPlaceholder.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-full w-full flex flex-col content-center justify-center opacity-50">
+    <q-icon :name="icon" size="24px" class="mx-auto" />
+    <div class="font-black italic">{{ text }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'EmptyPlaceholder',
+  props: {
+    icon: {
+      type: String,
+      default: 'information',
+    },
+    text: {
+      type: String,
+    },
+  },
+});
+</script>
