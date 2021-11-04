319bb5d2bff5d9e32140fe4c4e769d68f7cf5d0b
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-04 20:57:35 +0800

    :sparkles: Idle component

---
+++ src/pages/Pharmacist/Idle.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-item-label class="font-black" overline>IDLE</q-item-label>
+  <empty-placeholder icon="help" text="No prescription request selected." />
+</template>
+
+<script lang="ts">
+import EmptyPlaceholder from 'src/components/EmptyPlaceholder.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: { EmptyPlaceholder },
+  name: 'PharmacistView',
+});
+</script>
