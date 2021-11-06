c3b86eca9ec3b199bb00df31647ddc6a650ca77d
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-06 10:26:34 +0800

    :recycle: Wrap in q-page

--- src/pages/Pharmacist/Idle.vue
+++ src/pages/Pharmacist/Idle.vue
@@ -1,6 +1,8 @@
 <template>
-  <q-item-label class="font-black" overline>IDLE</q-item-label>
-  <empty-placeholder icon="help" text="No prescription request selected." />
+  <q-page>
+    <q-item-label class="font-black" overline>IDLE</q-item-label>
+    <empty-placeholder icon="help" text="No prescription request selected." />
+  </q-page>
 </template>
 
 <script lang="ts">
