53fca0967c48522944eb029cee397d4239d6c773
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-08 12:00:21 +0800

    :recycle: Refactor search API
    Search now abides by the composition API rules and has a name of useSearch, along with a fancy useNamedSearch function which you can use to retrieve a previously initialized search module.

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,24 +1,49 @@
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 import { api } from 'src/boot/axios';
 import { ResponseData } from 'src/models/ResponseData';
 import { Product } from 'src/models/Product';
 
-export const searchResults = ref<Product[]>([]);
-export const searchTerm = ref('');
-export const isLoading = ref(false);
+const namespaces: Record<string, ReturnType<typeof useSearch>> = {};
 
-watch(searchTerm, async (newSearchTerm) => {
-  searchResults.value = [];
-  searchResults.value = await search(newSearchTerm);
-});
+const useSearch = () => {
+  const searchResults = ref<Product[]>([]);
+  const searchTerm = ref('');
+  const searchIsLoading = ref(false);
+  const searchIsActive = computed(() => searchTerm.value.length > 2);
 
-export const search = async (searchTerm: string, pageNumber = 0): Promise<Product[]> => {
-  if (searchTerm == '') {
-    return [];
-  }
-  console.log('searching for', searchTerm);
-  isLoading.value = true;
-  const res = await api.post<ResponseData<Record<string, any>>>('/search', { searchTerm, pageNumber });
-  isLoading.value = false;
-  return res.data.data.results;
+  watch(searchTerm, async (newSearchTerm) => {
+    searchResults.value = [];
+    searchResults.value = await search(newSearchTerm);
+  });
+
+  /**
+   * Ideally you shouldn't call this on your own. There is an internal watcher for search term changes which then calls this.
+   */
+  const search = async (searchTerm: string, pageNumber = 0): Promise<Product[]> => {
+    if (searchTerm == '') {
+      return [];
+    }
+    console.log('searching for', searchTerm);
+    searchIsLoading.value = true;
+    const res = await api.post<ResponseData<Record<string, any>>>('/search', { searchTerm, pageNumber });
+    searchIsLoading.value = false;
+    return res.data.data.results;
+  };
+
+  return {
+    searchResults: computed(() => searchResults.value),
+    searchIsLoading: computed(() => searchIsLoading.value),
+    searchIsActive,
+    searchTerm,
+    search,
+  };
 };
+
+export const useNamedSearch = (namespace: string) => {
+  if (!namespaces[namespace]) {
+    namespaces[namespace] = useSearch();
+  }
+  return namespaces[namespace];
+};
+
+export default useSearch;
