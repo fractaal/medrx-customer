d9f43b7e7d24fa8881f8a4602af3d38ce0d80b87
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-11 02:45:35 +0800

    :construction: Scaffold out Restricted Users component

--- src/pages/Pharmacist/Restricted.vue
+++ src/pages/Pharmacist/Restricted.vue
@@ -1,9 +1,21 @@
-<template></template>
+<template>
+  <q-page>
+    <q-item-label overline class="font-black">RESTRICTED USERS</q-item-label>
+    <!-- Display currently restricted users: -->
+    <div class="grid grid-cols-5 gap-4"></div>
+  </q-page>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import * as restrictedUsers from 'src/api/pharmacist/restricted-users';
 
 export default defineComponent({
   name: 'PharmacistRestrictedUsers',
+  setup() {
+    return {
+      ...restrictedUsers,
+    };
+  },
 });
 </script>
