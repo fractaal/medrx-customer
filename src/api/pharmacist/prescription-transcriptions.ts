9eaa92e2791736fefe98a9f234ea04acfed71a6e
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-17 04:26:11 +0800

    :construction: add addItem & removeItem methods

--- src/api/pharmacist/prescription-transcriptions.ts
+++ src/api/pharmacist/prescription-transcriptions.ts
@@ -3,6 +3,9 @@ import { api } from 'src/boot/axios';
 import { ResponseData } from 'src/models/ResponseData';
 import { Product } from 'src/models/Product';
 import { CartItem } from 'src/models/CartItem';
+import { Notify } from 'quasar';
+
+// TODO: Rename this to just prescription submissions so that the serverside code naming makes sense.
 
 let lastCreatedPrescriptionUserId: string | null = null;
 
@@ -20,6 +23,26 @@ export const transcription = ref({
   extraRemarks: '',
 });
 
+export const addItemToTranscription = (product: Product) => {
+  if (transcription.value.cartItems.find((item) => item.productId === product.id)) {
+    Notify.create({ type: 'negative', message: 'This product is already in the cart!' });
+    return;
+  }
+
+  const cartItem = {
+    productId: product.id,
+    productName: product.name,
+    productQuantity: 1,
+    productPrice: product.price,
+  };
+  transcription.value.cartItems.push(cartItem);
+};
+
+export const removeItemFromTranscription = (product: Product) => {
+  const index = transcription.value.cartItems.findIndex((cartItem) => cartItem.productId === product.id);
+  transcription.value.cartItems.splice(index, 1);
+};
+
 export const createTranscriptionForUser = (userId: string, force = false) => {
   if (lastCreatedPrescriptionUserId === userId && !force) {
     return;
@@ -29,7 +52,17 @@ export const createTranscriptionForUser = (userId: string, force = false) => {
   lastCreatedPrescriptionUserId = userId;
 };
 
-export const submitTranscription = () => {
+export const submitTranscription = async () => {
+  if (transcription.value.cartItems.length === 0) {
+    Notify.create({ type: 'negative', message: 'Please add at least one product to the cart!' });
+    return;
+  }
+
+  if (transcription.value.userId === '') {
+    Notify.create({ type: 'negative', message: 'Please select a user!' });
+    return;
+  }
+
   return api.post<ResponseData<Record<string, Product>>>(`/transcription/${transcription.value.userId}`, {
     transcription: transcription.value,
   });
