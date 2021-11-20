aeebee28c71a8e578d4da40024f757955d499875
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-20 11:04:22 +0800

    🗃️ PrescriptionToConfirm model

---
+++ src/models/PrescriptionToConfirm.ts
@@ -0,0 +1,13 @@
+import { CartItem } from './CartItem';
+
+export interface PrescriptionToConfirm {
+  id: string;
+  userId: string;
+  dateUpdated?: any;
+  dateCreated: Date;
+  products: CartItem[];
+  isValid: boolean;
+  isConfirmed: boolean;
+  dateSubmitted: Date;
+  dateConfirmedOrCancelled?: any;
+}
