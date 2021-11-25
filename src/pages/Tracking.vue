e47f6986700f1f070659e3cc0833d0abab22df6d
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-26 02:08:08 +0800

    🚧 tracking page

---
+++ src/pages/Tracking.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <div class="text-h4">TRACKING</div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Tracking',
+});
+</script>
