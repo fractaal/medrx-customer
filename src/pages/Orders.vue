bb8516ca2008952ee5c667ca69a3d5ad4b573118
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-26 02:07:54 +0800

    🚧 orders page

---
+++ src/pages/Orders.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page>
+    <p v-if="ordersIsLoading">loading</p>
+    <div class="shadow-lg p-4" v-for="order in orders" :key="order.id">
+      {{ order }}
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import * as orders from 'src/api/orders';
+
+export default defineComponent({
+  name: 'Orders',
+  setup() {
+    onMounted(() => {
+      orders.getOrders();
+    });
+
+    return {
+      ...orders,
+    };
+  },
+});
+</script>
