7761ed963c9274ca7840f6e13b3488d35bba0510
Ben Jude <benedict.jude.rocat@gmail.com>
2021-11-28 02:42:15 +0800

    🚧 work in progress

--- src/pages/Tracking.vue
+++ src/pages/Tracking.vue
@@ -6,9 +6,27 @@
 
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue';
+import { Dialog } from 'quasar';
+import CoordinateSelectorDialog from 'src/components/CoordinateSelectorDialog.vue';
+import ProductDialog from 'src/components/ProductDialog.vue';
 
 export default defineComponent({
   name: 'Tracking',
-  setup() {},
+  setup() {
+    onMounted(() => {
+      setTimeout(() => {
+        Dialog.create({
+          component: CoordinateSelectorDialog,
+          persistent: true,
+          componentProps: {
+            titleText: 'Select a location',
+            descriptionText: 'Tell us where we need to deliver this order.',
+            confirmText: 'Deliver here',
+            cancelText: 'Prefer not to say',
+          },
+        });
+      }, 1000);
+    });
+  },
 });
 </script>
