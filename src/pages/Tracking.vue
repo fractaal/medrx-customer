6c35bf1df51e4212409ef653c6643bae57792d72
Ben Jude <benedict.jude.rocat@gmail.com>
2021-12-09 18:48:46 +0800

    💄 Design Tracking page

--- src/pages/Tracking.vue
+++ src/pages/Tracking.vue
@@ -1,52 +1,67 @@
 <template>
   <q-page>
-    <q-item-label class="text-h6 mb-4 font-black">Tracking</q-item-label>
-    <div id="map" ref="mapEl" class="w-full h-96 rounded-none sm:rounded-lg" />
-    <q-item-section class="my-2">
-      <q-skeleton
-        v-for="i in 10"
-        :key="i"
-        class="p-8"
-        :class="i === 10 ? 'bg-gray-300' : 'bg-transparent'"
-        style="display: flex"
-        :animation="i === 10 ? 'wave' : 'none'"
-      >
-        <span v-if="i === 10" class="absolute h-6 w-6 rounded-full bg-blue-500 animate-ping duration-75" />
-        <span class="absolute h-6 w-6 rounded-full" :class="i === 10 ? 'bg-blue-500' : 'bg-gray-600'" />
-        <span class="absolute h-[86px] -mt-8 w-0.5 ml-[11px] z-[-5] bg-gray-300" />
-        <p class="ml-12 font-medium">Waiting on a driver</p>
-      </q-skeleton>
+    <div>
+      <div id="map" ref="mapEl" class="w-full h-96 rounded-none sm:rounded-lg border boder-gray-200" />
+    </div>
+    <q-item-section class="my-2 relative">
+      <div class="absolute top-4 right-4 text-right -space-y-2">
+        <p class="font-bold">YOUR DRIVER IS</p>
+        <q-skeleton class="bg-transparent before:-mt-8" animation="wave">
+          <p class="font-bold text-4xl">2.1km</p>
+        </q-skeleton>
+        <p class="font-bold">AWAY</p>
+      </div>
+      <delivery-log
+        v-for="(log, i) in deliveryLogs"
+        :key="log.dateTime"
+        :deliveryLog="log"
+        :isActive="i === deliveryLogs.length - 1"
+      />
     </q-item-section>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref, watch } from 'vue';
-import { Dialog } from 'quasar';
-import CoordinateSelectorDialog from 'src/components/CoordinateSelectorDialog.vue';
-import ProductDialog from 'src/components/ProductDialog.vue';
-import { getDeliveryLocation } from 'src/api/delivery-location';
 import { useRouter } from 'vue-router';
+import * as delivery from 'src/api/delivery';
+import { formatDistanceToNow } from 'date-fns';
 
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { getDeliveryLocation } from 'src/api/delivery-location';
+import DeliveryLog from 'src/components/DeliveryLog.vue';
 
 export default defineComponent({
+  components: { DeliveryLog },
   name: 'Tracking',
   setup() {
     const router = useRouter();
     const mapEl = ref<HTMLElement>(null);
     let map: L.Map = null;
+    let deliveryLocationMarker: L.Marker = null;
 
-    const location = getDeliveryLocation();
+    const fallbackLocation = getDeliveryLocation();
+    const location: () => [number, number] = () => [
+      delivery.activeDeliveryLocation.value?.lat ?? fallbackLocation.lat,
+      delivery.activeDeliveryLocation.value?.lng ?? fallbackLocation.lng,
+    ];
+
+    watch(delivery.activeDeliveryLocation, () => {
+      if (!deliveryLocationMarker || !delivery.activeDeliveryLocation.value) return;
+      map.setView(location());
+    });
 
     watch(mapEl, (newMapEl) => {
       if (!newMapEl) return;
-      map = L.map('map').setView([location.lat, location.lng], 13);
+      map = L.map('map').setView(location(), 13);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-      const m = L.marker([location.lat, location.lng])
+      deliveryLocationMarker = L.marker([
+        delivery.activeDeliveryLocation.value?.lat ?? fallbackLocation.lat,
+        delivery.activeDeliveryLocation.value?.lng ?? fallbackLocation.lng,
+      ])
         .addTo(map)
         .setIcon(
           L.icon({
@@ -59,7 +74,7 @@ export default defineComponent({
           })
         );
 
-      m.bindPopup('<b>Your delivery location</b><br>').openPopup();
+      deliveryLocationMarker.bindPopup('<b>Your delivery location</b><br>').openPopup();
     });
 
     onMounted(async () => {
@@ -71,6 +86,8 @@ export default defineComponent({
 
     return {
       mapEl,
+      ...delivery,
+      formatDistanceToNow,
     };
   },
 });
