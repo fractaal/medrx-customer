f2c9ac51a4c8a63658a54fad172534429cbd83e8
lcsantos11395 <lcsantos11395@gmail.com>
2021-12-11 18:33:59 +0800

    :construction: Pushing cause cant pull HAHAHA.

--- src/api/profpicuploader.ts
+++ src/api/profpicuploader.ts
@@ -1,16 +1,36 @@
 
-import { getStorage, ref, uploadBytes } from "@firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from '@firebase/storage';
 import { getApp } from '@firebase/app';
 import { getAuth } from '@firebase/auth';
+import { LocalStorage } from 'quasar';
+import { ref as vueRef } from 'vue';
+import { seed } from 'src/api/seed';
+
+
+export const downloadURL = vueRef('');
+
+const storage = getStorage(getApp());
+const uploadDir = ref(storage, `users/${getAuth().currentUser?.uid}/profilePicture.png`);
+LocalStorage.set('downloadURL', getDownloadURL(uploadDir) ?? null);
+
+downloadURL.value = LocalStorage.getItem('downloadURL') ?? `https://avatars.dicebear.com/api/micah/${seed}.svg`;
+
+// get cloud storage reference
+
+
+
 
 export const uploadImage = (file: File) => {
 
-  //cloud storage reference
-  const storage = getStorage(getApp());
-  const uploadDir = ref(storage, `users/${getAuth().currentUser?.uid}/profilePicture.png`);
-
   uploadBytes(uploadDir, file);
+  const url = getDownloadURL(uploadDir);
+  LocalStorage.set('downloadURL', url);
 
 }
+
+
+
+
+
 
 
