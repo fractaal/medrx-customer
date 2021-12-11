14040aa1b74bf32db936890de5af9be5f84da05e
Ben Jude <benedict.jude.rocat@gmail.com>
2021-12-11 22:31:03 +0800

    ♻️ refactor vendor api due to vendor controller change

--- src/api/vendor.ts
+++ src/api/vendor.ts
@@ -1,19 +1,32 @@
 import { api } from 'src/boot/axios';
-import { Data } from 'src/models/Vendor';
+import { Products, Vendor } from 'src/models/Vendor';
 import { ResponseData } from 'src/models/ResponseData';
 import { ref } from 'vue';
 import { Notify } from 'quasar';
 
-export const isLoading = ref(false);
+export const vendorIsLoading = ref(false);
+export const productsIsLoading = ref(false);
 
-export const getVendor = async (vendorId: string, productsPageNumber = 0): Promise<Data | null> => {
-  isLoading.value = true;
-  const res = await api.get<ResponseData<Data>>('/vendor/' + vendorId + '/' + productsPageNumber.toString());
+export const getVendor = async (vendorId: string): Promise<Vendor | null> => {
+  vendorIsLoading.value = true;
+  const res = await api.get<ResponseData<Vendor>>('/vendor/' + vendorId);
   if (res.data.data == null) {
     Notify.create({ type: 'negative', message: 'There is no such vendor with the given ID.' });
-    isLoading.value = false;
+    vendorIsLoading.value = false;
     return null;
   }
-  isLoading.value = false;
+  vendorIsLoading.value = false;
+  return res.data.data;
+};
+
+export const getVendorProducts = async (vendorId: string, pageNumber = 0) => {
+  productsIsLoading.value = true;
+  const res = await api.get<ResponseData<Products>>(`/vendor/${vendorId}/products/${pageNumber}`);
+  if (res.data.data == null) {
+    Notify.create({ type: 'negative', message: 'There is no such vendor with the given ID.' });
+    productsIsLoading.value = false;
+    return null;
+  }
+  productsIsLoading.value = false;
   return res.data.data;
 };
