0e615915ea3034d0ea8e25162fe058e604070d92
Ben Jude <benedict.jude.rocat@gmail.com>
2021-12-12 14:12:54 +0800

    🐛 fix profile pic uploader

--- src/api/profpicuploader.ts
+++ src/api/profpicuploader.ts
@@ -1,4 +1,3 @@
-
 import { getStorage, ref, uploadBytes, getDownloadURL } from '@firebase/storage';
 import { getApp } from '@firebase/app';
 import { getAuth } from '@firebase/auth';
@@ -6,31 +5,22 @@ import { LocalStorage } from 'quasar';
 import { ref as vueRef } from 'vue';
 import { seed } from 'src/api/seed';
 
-
 export const downloadURL = vueRef('');
 
 const storage = getStorage(getApp());
 const uploadDir = ref(storage, `users/${getAuth().currentUser?.uid}/profilePicture.png`);
-LocalStorage.set('downloadURL', getDownloadURL(uploadDir) ?? null);
+
+getDownloadURL(uploadDir).then((url) => {
+  LocalStorage.set('downloadURL', url ?? null);
+});
+// LocalStorage.set('downloadURL', getDownloadURL(uploadDir) ?? null);
 
 downloadURL.value = LocalStorage.getItem('downloadURL') ?? `https://avatars.dicebear.com/api/micah/${seed}.svg`;
-
 // get cloud storage reference
 
-
-
-
-export const uploadImage = (file: File) => {
-
-  uploadBytes(uploadDir, file);
-  const url = getDownloadURL(uploadDir);
+export const uploadImage = async (file: File) => {
+  await uploadBytes(uploadDir, file);
+  const url = await getDownloadURL(uploadDir);
   LocalStorage.set('downloadURL', url);
-
-}
-
-
-
-
-
-
-
+  downloadURL.value = LocalStorage.getItem('downloadURL') ?? `https://avatars.dicebear.com/api/micah/${seed}.svg`;
+};
