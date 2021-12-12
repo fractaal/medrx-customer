74025c798790871e67978b57a17549166094fab7
Ben Jude <benedict.jude.rocat@gmail.com>
2021-12-12 14:12:38 +0800

    🚧 order API

--- src/api/order.ts
+++ src/api/order.ts
@@ -1,6 +1,17 @@
 // import { api } from 'src/boot/axios';
-// import { getCart } from './cart';
+// import { cart } from './cart';
+import { hasActiveDelivery } from './delivery';
+import { Notify } from 'quasar';
 
-// export const placeOrder = async () => {
-//   const cart = await getCart();
-// };
+export const placeOrder = () => {
+  if (hasActiveDelivery) {
+    Notify.create({
+      message: 'You cannot place an order while you have an active delivery!',
+      type: 'negative',
+    });
+    throw new Error('You cannot place an order while you have an active delivery!');
+    // return;
+  }
+
+  // TODO: Actually place an order
+};
